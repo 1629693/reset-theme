25007592c714dc3debcebadfda86ca8a1332d2fb
fuddl <a.mueller@loom-berlin.com>
2013-06-27 14:44:29 +0200

    msic

--- templates/views-view-grid.tpl.php
+++ templates/views-view-grid.tpl.php
@@ -16,11 +16,11 @@
 <?php if (!empty($caption)) : ?>
   <h2><?php print $caption; ?></h2>
 <?php endif; ?>
-<div class="<?php print $class; ?>"<?php print $attributes; ?>>
+<div class=" <?php print $class; ?>"<?php print $attributes; ?>>
 <?php foreach ($rows as $row_number => $columns): ?>
-  <div <?php if ($row_classes[$row_number]) { print 'class="row ' . $row_classes[$row_number] .'"';  } ?>>
+  <div <?php print 'class="row ' . $row_classes[$row_number] .'"'; ?>>
     <?php foreach ($columns as $column_number => $item): ?>
-      <div <?php if ($column_classes[$row_number][$column_number]) { print 'class="col ' . $column_classes[$row_number][$column_number] .'"';  } ?>>
+      <div <?php print 'class="col ' . $column_classes[$row_number][$column_number] .'"'; ?>>
         <?php print $item; ?>
       </div>
     <?php endforeach; ?>
